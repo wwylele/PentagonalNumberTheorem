765efde1e97d27fb167f11c862c4b80194d00225
Weiyi Wang <wwylele@gmail.com>
2025-09-12 19:51:47 -0400

    shift

--- PentagonalNumber/Approach2.lean
+++ PentagonalNumber/Approach2.lean
@@ -65,9 +65,10 @@ theorem WithPiTopology.summable_of_order_tendsto_atTop_atTop
 end PowerSeries
 
 noncomputable
-def AN_term (N n : ℕ) : PowerSeries ℤ := X ^ (N * n) * ∏ i ∈ Finset.range (n + 1), (1 - X ^ (N + i))
+def AN_term (N n : ℕ) : PowerSeries ℤ :=
+  X ^ ((N + 1) * n) * ∏ i ∈ Finset.range (n + 1), (1 - X ^ (N + 1 + i))
 
-theorem AN_summable (N : ℕ) (hN : 0 < N) : Summable (AN_term N) := by
+theorem AN_summable (N : ℕ) : Summable (AN_term N) := by
   rw [WithPiTopology.summable_iff]
   intro n
   apply summable_of_finite_support
@@ -76,17 +77,17 @@ theorem AN_summable (N : ℕ) (hN : 0 < N) : Summable (AN_term N) := by
   intro k h
   contrapose! h
   unfold AN_term
-  have : ¬ N * k ≤ n := by
+  have : ¬ (N + 1) * k ≤ n := by
     rw [not_le]
-    exact h.trans_le <| Nat.le_mul_of_pos_left k hN
+    exact h.trans_le <| Nat.le_mul_of_pos_left k (by simp)
   simp [PowerSeries.coeff_X_pow_mul', this]
 
 noncomputable
 def BN_term (N n : ℕ) : PowerSeries ℤ :=
-  X ^ (N * n) * (1 - X ^ N + X ^ (3 * N + n + 2) - X ^ (4 * N + 2 * n + 3)) *
-  ∏ i ∈ Finset.range n, (1 - X ^ (N + i + 1))
+  X ^ ((N + 1) * n) * (1 - X ^ (N + 1) + X ^ (3 * N + n + 5) - X ^ (4 * N + 2 * n + 7)) *
+  ∏ i ∈ Finset.range n, (1 - X ^ (N + i + 2))
 
-theorem AN_BN (N n : ℕ) : AN_term N n + X ^ (3 * N + 2) * AN_term (N + 1) n = BN_term N n := by
+theorem AN_BN (N n : ℕ) : AN_term N n + X ^ (3 * N + 5) * AN_term (N + 1) n = BN_term N n := by
   unfold BN_term
   rw [add_sub_assoc]
   rw [mul_add, add_mul]
@@ -102,9 +103,9 @@ theorem AN_BN (N n : ℕ) : AN_term N n + X ^ (3 * N + 2) * AN_term (N + 1) n = 
 
 noncomputable
 def CN (N n : ℕ) : PowerSeries ℤ :=
-  X ^ (N * n) * (X ^ (2 * N + n + 1) - 1) * ∏ i ∈ Finset.range n, (1 - X ^ (N + i + 1))
+  X ^ ((N + 1) * n) * (X ^ (2 * N + n + 3) - 1) * ∏ i ∈ Finset.range n, (1 - X ^ (N + i + 2))
 
-theorem coeff_CN {N n k : ℕ} (h : k < N * n) : coeff k (CN N n) = 0 := by
+theorem coeff_CN {N n k : ℕ} (h : k < (N + 1) * n) : coeff k (CN N n) = 0 := by
   unfold CN
   rw [mul_assoc]
   apply PowerSeries.coeff_of_lt_order
@@ -119,11 +120,11 @@ theorem BN_CN (N n : ℕ) : BN_term N n = CN N (n + 1) - CN N n := by
   unfold BN_term
   ring_nf
 
-theorem AN_rec (N : ℕ) (hN : 0 < N) (u : PowerSeries ℤ) (h : HasSum (AN_term (N + 1)) u) :
-    HasSum (AN_term N) (1 - X ^ (2 * N + 1) - X ^ (3 * N + 2) * u) := by
-  suffices HasSum (fun n ↦ AN_term N n + X ^ (3 * N + 2) * AN_term (N + 1) n)
-      (1 - X ^ (2 * N + 1)) by
-    simpa using this.sub (h.mul_left (X ^ (3 * N + 2)))
+theorem AN_rec (N : ℕ) (u : PowerSeries ℤ) (h : HasSum (AN_term (N + 1)) u) :
+    HasSum (AN_term N) (1 - X ^ (2 * N + 3) - X ^ (3 * N + 5) * u) := by
+  suffices HasSum (fun n ↦ AN_term N n + X ^ (3 * N + 5) * AN_term (N + 1) n)
+      (1 - X ^ (2 * N + 3)) by
+    simpa using this.sub (h.mul_left (X ^ (3 * N + 5)))
   simp_rw [AN_BN, BN_CN]
   unfold HasSum
   rw [PowerSeries.WithPiTopology.tendsto_iff_coeff_tendsto]
@@ -138,23 +139,23 @@ theorem AN_rec (N : ℕ) (hN : 0 < N) (u : PowerSeries ℤ) (h : HasSum (AN_term
     simp only [Finset.sum_sub_distrib, map_sub, map_add, map_sum]
   apply sub_eq_of_eq_add
   trans 0
-  · rw [coeff_CN ((Nat.lt_add_one n).trans_le (by simpa using hN))]
+  · rw [coeff_CN ((Nat.lt_add_one n).trans_le (by simp))]
     rw [add_zero, sub_eq_zero]
     trans 0
     · apply Finset.sum_eq_zero
       simp_rw [Finset.mem_sdiff, Finset.mem_range, not_lt, and_imp, Nat.add_one_le_iff]
       intro k _ hk
-      rw [coeff_CN (hk.trans ((Nat.lt_add_one k).trans_le (by simpa using hN)))]
+      rw [coeff_CN (hk.trans ((Nat.lt_add_one k).trans_le (by simp)))]
     · symm
       apply Finset.sum_eq_zero
       simp_rw [Finset.mem_sdiff, Finset.mem_range, not_lt, and_imp, Nat.add_one_le_iff]
       intro k _ hk
-      rw [coeff_CN (hk.trans_le (Nat.le_mul_of_pos_left _ (by simpa using hN)))]
+      rw [coeff_CN (hk.trans_le (Nat.le_mul_of_pos_left _ (by simp)))]
   · rw [← map_add, CN]
     convert (map_zero (coeff n (R := ℤ))).symm
     ring_nf
 
-theorem A1 (u : PowerSeries ℤ) (h : HasSum (AN_term 1) u) :
+theorem A0 (u : PowerSeries ℤ) (h : HasSum (AN_term 0) u) :
     HasProd (fun n ↦ 1 - X ^ (n + 1)) (1 - X - X ^ 2 * u) := by
   unfold HasProd
   rw [PowerSeries.WithPiTopology.tendsto_iff_coeff_tendsto]
@@ -196,7 +197,7 @@ theorem A1 (u : PowerSeries ℤ) (h : HasSum (AN_term 1) u) :
     · apply Finset.sum_congr rfl
       intro k _
       unfold AN_term
-      rw [one_mul]
+      rw [zero_add, one_mul]
       by_cases hk : k ≤ n
       · apply congr((coeff n) (X ^ k * Finset.prod $_ $_))
         · rw [show k + 1 = 1 + k by ring]
@@ -245,56 +246,56 @@ theorem A1 (u : PowerSeries ℤ) (h : HasSum (AN_term 1) u) :
       simp at h
     simp [coeff_X_pow, hxy0, hxyy]
 
-theorem pentagonal_rec {n : ℕ} (hn : 0 < n) :
+theorem pentagonal_rec {n : ℕ} :
     ∀ {u : PowerSeries ℤ}, HasSum (AN_term n) u →
     HasProd (fun n ↦ 1 - X ^ (n + 1))
-    (1 + ∑ k ∈ Finset.range n, ((-1) ^ (k + 1) *
+    (1 + ∑ k ∈ Finset.range (n + 1), ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) +
-      X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (u - 1)) := by
-  induction n, hn using Nat.le_induction with
-  | base =>
+      X ^ ((n + 1) * (3 * n + 4) / 2) * (-1) ^ (n + 1) * (u - 1)) := by
+  induction n with
+  | zero =>
     intro u h
-    convert A1 u h using 1
+    convert A0 u h using 1
     simp
     ring_nf
-  | succ n hn ih =>
+  | succ n ih =>
     intro u h
-    obtain h := ih <| AN_rec _ hn _ h
+    obtain h := ih <| AN_rec _ _ h
     convert h using 1
     rw [Finset.sum_range_succ]
-    simp_rw [add_assoc]
-    congr
-    have : ((-1) ^ (n + 1)) = (-(-1) ^ n : ℤ⟦X⟧) := by
+    have : ((-1) ^ (n + 1 + 1)) = (-(-1) ^ (n + 1) : ℤ⟦X⟧) := by
       rw [npow_add]
       simp
     simp_rw [this]
-    have : 3 * (n + 1) + 1 = 3 * n + 4 := by ring
+    simp_rw [add_assoc]
+    congr
+    have : 3 * (n + 1) + 4 = 3 * n + 7 := by ring
     simp_rw [this]
     conv =>
       right
-      rw [mul_comm (X ^ (n * (3 * n + 1) / 2)), mul_assoc]
+      rw [mul_comm (X ^ ((n + 1) * (3 * n + 4) / 2)), mul_assoc]
       rw [sub_right_comm _ _ 1, sub_right_comm _ _ 1, sub_self, zero_sub]
-      rw [← neg_add', mul_neg, mul_neg, ← neg_mul, mul_add (X ^ (n * (3 * n + 1) / 2))]
+      rw [← neg_add', mul_neg, mul_neg, ← neg_mul, mul_add (X ^ ((n + 1) * (3 * n + 4) / 2))]
       rw [← mul_assoc _ _ u]
       rw [← pow_add, ← pow_add]
       rw [← Nat.add_mul_div_left _ _ (by simp), ← Nat.add_mul_div_left _ _ (by simp)]
     conv =>
       left
-      rw [mul_comm (X ^ ((n + 1) * (3 * n + 4) / 2)), mul_assoc]
+      rw [mul_comm (X ^ ((n + 2) * (3 * n + 7) / 2)), mul_assoc]
       rw [← mul_add, mul_sub_one, add_add_sub_cancel]
     apply congr(_ * (X ^ ($_ / 2) + X ^ ($_ / 2) * u))
     · ring
     · ring
 
-theorem pentagonal_const {m n : ℕ} (hm : 0 < m) (hn : 0 < n) {u v : ℤ⟦X⟧}
+theorem pentagonal_const {m n : ℕ} {u v : ℤ⟦X⟧}
     (hu : HasSum (AN_term m) u) (hv : HasSum (AN_term n) v) :
-    (1 + ∑ k ∈ Finset.range m, ((-1) ^ (k + 1) *
+    (1 + ∑ k ∈ Finset.range (m + 1), ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) +
-      X ^ (m * (3 * m + 1) / 2) * ((-1) ^ m) * (u - 1)) =
-    (1 + ∑ k ∈ Finset.range n, ((-1) ^ (k + 1) *
+      X ^ ((m + 1) * (3 * m + 4) / 2) * ((-1) ^ (m + 1)) * (u - 1)) =
+    (1 + ∑ k ∈ Finset.range (n + 1), ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) +
-      X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (v - 1)) :=
-  HasProd.unique (pentagonal_rec hm hu) (pentagonal_rec hn hv)
+      X ^ ((n + 1) * (3 * n + 4) / 2) * (-1) ^ (n + 1) * (v - 1)) :=
+  HasProd.unique (pentagonal_rec hu) (pentagonal_rec hv)
 
 theorem term_order (k : ℕ) : ((-1 : ℤ⟦X⟧) ^ (k + 1) *
     (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))).order =
@@ -318,19 +319,20 @@ theorem term_order (k : ℕ) : ((-1 : ℤ⟦X⟧) ^ (k + 1) *
 theorem pentagonal_lim (u : ℤ⟦X⟧) (v : ℕ → ℤ⟦X⟧)
     (hu : HasSum (fun (k : ℕ) ↦ ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2)))) u) :
-    Filter.Tendsto (fun n ↦ (1 + ∑ k ∈ Finset.range n, ((-1) ^ (k + 1) *
+    Filter.Tendsto (fun n ↦ (1 + ∑ k ∈ Finset.range (n + 1), ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) +
-      X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (v n - 1)))
+      X ^ ((n + 1) * (3 * n + 4) / 2) * (-1) ^ (n + 1) * (v n - 1)))
       Filter.atTop (nhds (1 + u)) := by
   rw [WithPiTopology.tendsto_iff_coeff_tendsto]
   intro n
-  apply tendsto_atTop_of_eventually_const (i₀ := (n + 1))
+  apply tendsto_atTop_of_eventually_const (i₀ := n)
   intro i hi
   simp_rw [map_add]
   rw [mul_assoc]
-  have hni : n < i * (3 * i + 1) / 2 := by
-    rw [ge_iff_le, Nat.add_one_le_iff] at hi
-    apply hi.trans_le
+  have hni : n < (i + 1) * (3 * i + 4) / 2 := by
+    rw [ge_iff_le] at hi
+    apply hi.trans_lt
+    apply Nat.lt_of_add_one_le
     rw [Nat.le_div_iff_mul_le (by simp)]
     obtain rfl | h0 := Nat.eq_zero_or_pos i
     · simp
@@ -342,7 +344,7 @@ theorem pentagonal_lim (u : ℤ⟦X⟧) (v : ℕ → ℤ⟦X⟧)
   rw [WithPiTopology.has_sum_iff] at hu
   specialize hu n
   refine tendsto_nhds_unique ?_ hu
-  apply tendsto_atTop_of_eventually_const (i₀ := Finset.range i)
+  apply tendsto_atTop_of_eventually_const (i₀ := Finset.range (i + 1))
   intro s hs
   rw [← Finset.sum_sdiff hs, add_eq_right]
   apply Finset.sum_eq_zero
@@ -358,42 +360,22 @@ theorem pentagonal_lim (u : ℤ⟦X⟧) (v : ℕ → ℤ⟦X⟧)
 theorem pentagonal_lim_eq {u : ℤ⟦X⟧} {v : ℕ → ℤ⟦X⟧}
     (hu : HasSum (fun (k : ℕ) ↦ ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2)))) u)
-    {n : ℕ} (hn : 0 < n) (hv : ∀ n, 0 < n → HasSum (AN_term n) (v n)) :
-    (1 + ∑ k ∈ Finset.range n, ((-1) ^ (k + 1) *
+    {n : ℕ} (hv : ∀ n, HasSum (AN_term n) (v n)) :
+    (1 + ∑ k ∈ Finset.range (n + 1), ((-1) ^ (k + 1) *
       (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) +
-      X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (v n - 1)) =
+      X ^ ((n + 1) * (3 * n + 4) / 2) * (-1) ^ (n + 1) * (v n - 1)) =
     1 + u := by
   obtain hlim := pentagonal_lim u v hu
-
-  have : (fun n ↦ 1 +
-        ∑ k ∈ Finset.range n,
-          (-1) ^ (k + 1) * (X ^ ((k + 1) * (3 * k + 2) / 2) +
-          X ^ ((k + 1) * (3 * k + 4) / 2)) +
-        X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (v n - 1)) =ᶠ[Filter.atTop]
-      (fun _ ↦ 1 +
-        ∑ k ∈ Finset.range n,
-          (-1) ^ (k + 1) * (X ^ ((k + 1) * (3 * k + 2) / 2) +
-          X ^ ((k + 1) * (3 * k + 4) / 2)) +
-        X ^ (n * (3 * n + 1) / 2) * (-1) ^ n * (v n - 1)) := by
-    unfold Filter.EventuallyEq
-    rw [Filter.eventually_atTop]
-    use 1
-    intro m hm
-    simp only
-    apply pentagonal_const hm hn (hv m hm) (hv n hn)
-  rw [Filter.tendsto_congr' this] at hlim
+  simp_rw [fun m ↦ pentagonal_const (hv m) (hv n)] at hlim
   exact tendsto_nhds_unique tendsto_const_nhds hlim
 
 theorem pentagonal_inf (u : ℤ⟦X⟧) (hu : HasSum (fun (k : ℕ) ↦ (-1) ^ (k + 1) *
     (X ^ ((k + 1) * (3 * k + 2) / 2) + X ^ ((k + 1) * (3 * k + 4) / 2))) u) :
     HasProd (fun n ↦ 1 - X ^ (n + 1)) (1 + u) := by
   choose f hf using AN_summable
-  let f' (n : ℕ) := if h : 0 < n then f n h else 0
-  have hf' (n : ℕ) (hn : 0 < n) : HasSum (AN_term n) (f' n) := by
-    simpa [f', hn] using hf n
-  obtain h := pentagonal_rec (show 0 < 1 by simp) (hf' 1 (by simp))
+  obtain h := pentagonal_rec (hf 1)
   convert ← h using 1
-  exact pentagonal_lim_eq hu (by simp) hf'
+  exact pentagonal_lim_eq hu hf
 
 theorem summable_pentagonal :
     Summable (fun (k : ℕ) ↦ ((-1 : ℤ⟦X⟧) ^ (k + 1) *
